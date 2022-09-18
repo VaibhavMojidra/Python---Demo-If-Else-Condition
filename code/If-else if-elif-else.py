0e6482f85452b05dfd5ee8456c183ec2a582fb56
VaibhavMojidra <vmojidra25@gmail.com>
2022-09-19 01:48:26 +0530

    If-else Condition

---
+++ code/If-else if-elif-else.py
@@ -0,0 +1,24 @@
+#If-else/elif
+
+num1=20
+if(num1>3):
+    print(str(num1)+" is greater than 3")
+
+
+num2=16
+if(num1>num2):
+    print(str(num1)+" is greater than "+str(num2))
+else:
+    print(str(num1)+" is smaller than "+str(num2))
+
+num1=17
+num2=17
+if(num1>num2):
+    print(str(num1)+" is greater than "+str(num2))
+elif(num1==num2):
+    print(str(num1)+" is equals to  "+str(num2))
+else:
+    print(str(num1)+" is smaller than "+str(num2))
+
+
+
